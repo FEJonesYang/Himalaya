d20fc3e71ba7b68a18af2e4d47b31fa036be0974
Yangchao <545661033@qq.com>
2020-10-12 11:35:58 +0800

    init commit

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    int size = categories.size();
+                    Log.d(TAG, "categories size --> " + size);
+                    for (Category category : categories) {
+                        Log.d(TAG, "category code -->" + category.getCategoryName());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.d(TAG, "error code --> " + i + ",error messages -->" + s);
+            }
+        });
     }
 }
